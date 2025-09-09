280ea874226405f2607c73c81c51ee2acd5a450a
Cinders <cinders.pear@gmail.com>
2025-09-09 00:53:17 -0700

    Add files via upload

---
+++ global.gd
@@ -0,0 +1,52 @@
+extends Node
+
+var is_dragging = false;
+var spark_1 = false;
+var spark_2 = false;
+var spark_3 = false;
+
+var sprout_1 = false;
+var sprout_2 = false;
+var sprout_3 = false;
+
+var crescent_1 = false;
+var crescent_2 = false;
+var crescent_3 = false;
+
+var pear_1 = false;
+var pear_2 = false;
+var pear_3 = false;
+
+var star_1 = false;
+var star_2 = false;
+var star_3 = false;
+
+var halfmoon_1 = false;
+var halfmoon_2 = false;
+var halfmoon_3 = false;
+
+var cat_1 = false;
+var cat_2 = false;
+var cat_3 = false;
+
+var fullishmoon_1 = false;
+var fullishmoon_2 = false;
+var fullishmoon_3 = false;
+
+var wand_1 = false;
+var wand_2 = false;
+var wand_3 = false;
+
+var catsprout_1 = false;
+var catsprout_2 = false;
+var catsprout_3 = false;
+var catsprout_4 = false;
+var catsprout_5 = false;
+var catsprout_6 = false;
+var catsprout_7 = false;
+var catsprout_8 = false;
+var catsprout_9 = false;
+
+var catpear_1 = false;
+var hat_1 = false;
+var fullmoon_1 = false;
